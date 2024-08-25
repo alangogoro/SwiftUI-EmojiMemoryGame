5846a06e5011c49c1a5213ed687593f05d38e7a5
alangogoro <cayne9160@gmail.com>
2024-08-25 21:44:06 +0800

    Initial Commit CardView

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,16 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
         }
         .padding()
     }
+
 }
+
+// MARK: 卡片視圖
+struct CardView: View {
+    var isFaceUp: Bool = false
+
+    var body: some View {
+        
+        // Functional Programming 將函式作為參數傳遞
+        // 比如下VStack的 content 參數
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+        .padding()               // Modifier
+        .foregroundColor(.orange)// Modifier
+
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
