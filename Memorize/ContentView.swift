2ce49e76c9152cc9ce573dd8e58573695367a647
alangogoro <cayne9160@gmail.com>
2024-08-27 00:58:56 +0800

    Add Buttons and Put Cards in LazyGrid

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,18 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "🎃", "😈"]
+    let emojis = ["👻", "🎃", "😈", "💀", "🧙🏻‍♀️", "🕸️", "👺"]
+    @State var cardCount: Int = 3
 
     var body: some View {
-        HStack {
-            // ForEach View
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+        VStack {
+            ScrollView {
+                // LazyGride 會盡可能內縮
+                // GridItem 可以設置欄位大小
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+                    // ForEach View
+                    ForEach(0..<cardCount, id: \.self) { index in
+                        CardView(content: emojis[index])
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+                .foregroundColor(.orange)// Modifier
             }
+
+            Spacer()
+
+            HStack {
+                cardAdder
+                Spacer()
+                cardRemover
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
+
         }
         .padding()
     }
-
+    
+    /// 加卡片按鈕
+    var cardAdder: some View {
+        Button {
+            cardCount += 1
+        } label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+        }
+        // 在 … 情況下設為禁用
+        .disabled(cardCount > emojis.count - 1)
+    }
+    
+    /// 減卡片按鈕
+    var cardRemover: some View {
+        Button {
+            cardCount -= 1
+        } label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+        }
+        // 在 … 情況下設為禁用
+        .disabled(cardCount <= 1)
+    }
 }
 
 // MARK: 卡片視圖
@@ -28,20 +69,20 @@ struct CardView: View {
     @State var isFaceUp = false
 
     var body: some View {
-        
+
         // Functional Programming 將函式作為參數傳遞
         // 比如下VStack的 content 參數
         ZStack(content: {
             let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
-                base.foregroundColor(.white)
+            Group {
+                base.fill().foregroundColor(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base.fill()
             }
+            // opacity 透明度
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
         })
-        .foregroundColor(.orange)// Modifier
         // 點擊 ZStack 時觸發
         .onTapGesture(perform: {
             isFaceUp.toggle()
